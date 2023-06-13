d934c363809e045feaef58854a3e7b2e606b9cd1
Dieter Tack <dieter.tack@outlook.com>
2023-06-13 23:30:54 +0200

    added colemak layout & some small adjustments

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -1,10 +1,11 @@
 #include "quantum.h"
 #include <stdio.h>
 
-#define L_BASE 0
-#define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
-#define L_ADJUST (1 << 3)
+#define L_QWERTY 0
+#define L_COLEMAK (1 << 1)
+#define L_LOWER (1 << 2)
+#define L_RAISE (1 << 3)
+#define L_ADJUST (1 << 4)
 #define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
 
 char layer_state_str[24];
@@ -12,8 +13,11 @@ char layer_state_str[24];
 const char *read_layer_state(void) {
   switch (layer_state)
   {
-  case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+  case L_QWERTY:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Qwerty");
+    break;
+  case L_COLEMAK:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Colemak");
     break;
   case L_RAISE:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
