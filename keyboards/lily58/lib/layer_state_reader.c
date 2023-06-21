82b413d2d6b98b5c900f9b7c7cd021aa734ae21f
Dieter Tack <dieter@warcave.com>
2023-06-21 21:47:10 +0200

    add layered numpad + make colemak default

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -1,37 +1,38 @@
 #include "quantum.h"
 #include <stdio.h>
 
-#define L_QWERTY 0
-#define L_COLEMAK (1 << 1)
-#define L_LOWER (1 << 2)
-#define L_RAISE (1 << 3)
-#define L_ADJUST (1 << 4)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+enum layer_number {  
+  _COLEMAK = 0,
+  _QWERTY,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _ADJUST_TRI = _ADJUST | _RAISE | _LOWER
+};
 
 char layer_state_str[24];
 
 const char *read_layer_state(void) {
-  switch (layer_state)
-  {
-  case L_QWERTY:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Qwerty");
-    break;
-  case L_COLEMAK:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Colemak");
-    break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
-    break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
-    break;
-  case L_ADJUST:
-  case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
-    break;
-  default:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
-  }
+    switch (get_highest_layer(layer_state | default_layer_state)) {
+        case _COLEMAK:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Colemak");
+            break;
+        case _QWERTY:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Qwerty");
+            break;
+        case _RAISE:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+            break;
+        case _LOWER:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+            break;
+        case _ADJUST:
+        case _ADJUST_TRI:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+            break;
+        default:
+            snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
+    }
 
-  return layer_state_str;
+    return layer_state_str;
 }
