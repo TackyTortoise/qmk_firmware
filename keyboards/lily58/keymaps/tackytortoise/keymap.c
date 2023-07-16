cf0a38477e42a777458c7bbe6f619093c542d7e0
Dieter Tack <dieter@warcave.com>
2023-07-16 21:32:38 +0200

    accidentally removed 0

--- keyboards/lily58/keymaps/tackytortoise/keymap.c
+++ keyboards/lily58/keymaps/tackytortoise/keymap.c
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|---+---                    |------+------+------+------+------+------|
  * |      |      |      |   (  |   )  |   [  |-------.    ,-------|   ^  |   +  |   =  |   -  |   *  |      |
  * |------+------+------+------+------|---+---   [   |    |    ]  |------+------+------+------+------+------|
- * |      |      |      |   {  |   }  |   ]  |-------|    |-------|      |   #  |   `  |      |   \  |      |
+ * |      |      |      |   {  |   }  |   ]  |-------|    |-------|   @  |   #  |   `  |      |   \  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LGUI |LOWER | /Space  /       \Delete\  |RAISE |BackSP| RGUI |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______,   _______,   _______,                   _______,   _______,   _______,   _______,  _______,  _______,
   _______, _______, _______, KC_LT,    KC_GT, _______,                     _______, KC_EXLM, KC_AMPR,KC_PIPE, KC_TILD, _______,
   KC_LSFT, _______, _______, KC_LPRN, KC_RPRN, KC_LBRC ,                   KC_CIRC, KC_PLUS, KC_EQL, KC_MINS, KC_ASTR, _______,
-  KC_LCTL, _______, _______, KC_LCBR, KC_RCBR, KC_RBRC,  _______, _______, XXXXXXX, KC_HASH, KC_GRV, _______, KC_BSLS, _______,
+  KC_LCTL, _______, _______, KC_LCBR, KC_RCBR, KC_RBRC,  _______, _______, KC_AT  , KC_HASH, KC_GRV, _______, KC_BSLS, _______,
                              _______, _______, _______, _______, KC_DEL,  _______, _______, _______
 ),
 /* RAISE
@@ -122,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   KC_GRV,  _______,  _______,   KC_7,   KC_8,   KC_9,                    _______, KC_HOME, KC_UP,   KC_END, _______,  KC_F12,
   KC_LSFT, _______,  _______,   KC_4,   KC_5,   KC_6,                    XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,_______, XXXXXXX,
-  KC_LCTL, _______,  _______,   KC_1,   KC_2,   KC_3, _______, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+  KC_LCTL, _______,  _______,   KC_1,   KC_2,   KC_3,    KC_0, _______,  KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
                        KC_PRINT_SCREEN, KC_LALT, _______,  _______ , _______,  _______, _______, _______
 ),
 /* ADJUST
